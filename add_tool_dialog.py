63fe90281373045cc676f2547a8735a35e5adc51
Mashiro-Yukino <2636481032x@gmail.com>
2023-04-24 14:15:52 -0400

    Update add_tool_dialog.py

--- add_tool_dialog.py
+++ add_tool_dialog.py
@@ -1,67 +1,8 @@
-#!/usr/bin/env python3
-# -*- coding: utf-8 -*-
-"""
-Created on Fri Apr 21 10:43:47 2023
-
-@author: mu
-"""
-
-
-from PyQt5.QtWidgets import QDialog, QVBoxLayout, QLabel, QLineEdit, QPushButton
 import config
 import pickle
 import sqlite3
-
-# class AddToolDialog(QDialog):
-#     def __init__(self, parent=None):
-#         super(AddToolDialog, self).__init__(parent)
-#
-#         layout = QVBoxLayout()
-#
-#         self.name_label = QLabel("Tool Name")
-#         layout.addWidget(self.name_label)
-#         self.name_input = QLineEdit()
-#         layout.addWidget(self.name_input)
-#
-#         self.keywords_label = QLabel("Keywords (comma separated)")
-#         layout.addWidget(self.keywords_label)
-#         self.keywords_input = QLineEdit()
-#         layout.addWidget(self.keywords_input)
-#
-#         self.save_button = QPushButton("Save Tool")
-#         self.save_button.clicked.connect(self.save_tool)
-#         layout.addWidget(self.save_button)
-#
-#         self.setLayout(layout)
-#
-#     def save_tool(self):
-#         tool_name = self.name_input.text().strip()
-#         tool_keywords = [kw.strip()
-#                          for kw in self.keywords_input.text().split(',')]
-#
-#         new_tool = {
-#             "name": tool_name,
-#             "keywords": tool_keywords
-#         }
-#
-#         config.AI_tools.append(new_tool)
-#
-#         with open('ai_tools.pkl', 'wb') as f:
-#             pickle.dump(config.AI_tools, f)
-#
-#         self.accept()
-#
-#     def add_ai_tool(self, name, tool_type, keywords):
-#         conn = sqlite3.connect("ai_tools.db")
-#         c = conn.cursor()
-#         c.execute("INSERT INTO ai_tools (name, type, keywords) VALUES (?, ?, ?)",
-#                   (name, tool_type, ', '.join(keywords)))
-#         conn.commit()
-#         conn.close()
-
-
 from PyQt5.QtWidgets import QDialog, QVBoxLayout, QLabel, QPushButton, QHBoxLayout, QLineEdit, QComboBox, QFormLayout, QMessageBox
-import sqlite3  # Add this line at the beginning of the file
+
 
 class AddToolDialog(QDialog):
     def __init__(self, parent=None):
@@ -100,12 +41,14 @@ class AddToolDialog(QDialog):
         keywords = self.keywords_input.text().strip().split(',')
 
         if not name or not keywords:
-            QMessageBox.warning(self, "Incomplete Information", "Please fill in the required fields.")
+            QMessageBox.warning(self, "Incomplete Information",
+                                "Please fill in the required fields.")
             return
 
         conn = sqlite3.connect("ai_tools.db")
         c = conn.cursor()
-        c.execute("INSERT INTO ai_tools (name, type, keywords) VALUES (?, ?, ?)", (name, tool_type, ', '.join(keywords)))
+        c.execute("INSERT INTO ai_tools (name, type, keywords) VALUES (?, ?, ?)",
+                  (name, tool_type, ', '.join(keywords)))
         conn.commit()
         conn.close()
 
